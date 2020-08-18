54888c7d4d7755e1bd5ae4497caa16562263d88d
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-08-17 22:09:30 -0300

    Add User page component & functionality

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,10 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import { View } from 'react-native';
+import PropTypes from 'prop-types';
 
-function User({ route }) {
-  console.tron.log(route.params);
+import api from '../../services/api';
 
-  return <View />;
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author
+} from './styles';
+
+function User({ navigation, route }) {
+  const [stars, setStars] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: route.params.user.name
+    });
+  }, []);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      setLoading(true);
+      const { user } = route.params;
+      const response = await api.get(`/users/${user.login}/starred`);
+      setStars(response.data);
+      setLoading(false);
+    };
+
+    loadUser();
+  }, []);
+
+  const { user } = route.params;
+
+  return (
+    <Container>
+      <Header>
+        <Avatar source={{ uri: user.avatar }} />
+        <Name>{user.name}</Name>
+        <Bio>{user.bio}</Bio>
+      </Header>
+
+      {loading ? (
+        <View />
+      ) : (
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      )}
+    </Container>
+  );
+};
+
+User.propTypes = {
+  navigation: PropTypes.shape({
+    setOptions: PropTypes.func,
+  }).isRequired,
+  route: PropTypes.shape({
+    params: PropTypes.object,
+  }).isRequired,
 };
 
 export default User;
